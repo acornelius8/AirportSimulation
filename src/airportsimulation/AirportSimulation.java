6dd6cc73a69ad21ecf244479e2eccb54e75f3765
Andy Cornelius <acornelius@stu.jsu.edu>
2019-03-21 16:15:09 -0500

    Added extra comments

--- src/airportsimulation/AirportSimulation.java
+++ src/airportsimulation/AirportSimulation.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class AirportSimulation {
 
     public static void main(String[] args) {
-        Queue<Integer> takeoffQueue = new LinkedList<>();
-        Queue<Integer> landingQueue = new LinkedList<>();
+        Queue<Integer> takeoffQueue = new LinkedList<>();        //Queue for Takeoff
+        Queue<Integer> landingQueue = new LinkedList<>();        //Queue for Landing
         Boolean x = true;
         
         takeoffQueue.add(1);
@@ -13,7 +13,7 @@ public class AirportSimulation {
         while(x)
         {
             Random rand = new Random();
-            int randNum = rand.nextInt(100) + 1;
+            int randNum = rand.nextInt(100) + 1;           //Determines a random number between 1 and 100
             
             //Adds a plane to the takeoff or landing queue or does nothing
             if(randNum >= 1 && randNum < 50 && takeoffQueue.size() != 10)
@@ -21,13 +21,11 @@ public class AirportSimulation {
                 takeoffQueue.add(randNum);
                 System.out.println("A plane is waiting to takeoff");
             }
-            else if (randNum >= 50 && randNum < 100 && landingQueue.size() != 10)
+            else if (randNum >= 50 && randNum <= 100 && landingQueue.size() != 10)
             {
                 landingQueue.add(randNum);
                 System.out.println("A plane is ready to land");
             }
-            else
-                ;
             
             //Determines whether a plane can takeoff or land
             if(landingQueue.size() >= 1)
@@ -41,6 +39,7 @@ public class AirportSimulation {
                 System.out.println("A plane is taking off");
             }
             
+            //Set x = false when the takeoff queue is empty, ending the simulation
             if (takeoffQueue.isEmpty())
                 x = false;
         }
